78b73264db2576bae844b12845074b5ace8eb262
Gurkirat Saini <78436028+GurkiratSaini@users.noreply.github.com>
2022-03-01 15:50:54 -0500

    set up project structure, added dependencies, adding server files

---
+++ server/models/User.js
@@ -0,0 +1,47 @@
+const mongoose = require("mongoose");
+const Order = require("./Order");
+const bcrypt = require("bcrypt");
+
+const { Schema } = mongoose;
+
+const userSchema = new Schema({
+  firstName: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    minLength: 8,
+  },
+  orders: [Order.schema],
+});
+
+//pre-save middleware to salt password
+userSchema.pre("save", async function (next) {
+  if (this.isNew || this.isModified("password")) {
+    const saltRounds = 10;
+    this.password = await bcrypt.hash(this.password, saltRounds);
+  }
+  next();
+});
+
+// compare incoming password with hashed password
+userSchema.methods.isCorrectPassword = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
